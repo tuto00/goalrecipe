618d446d450d087f23adb5726f16ff2a1ed92c47
tsutomu <matsuo0215@gmail.com>
2016-11-13 20:31:54 +0900

    first commit

---
+++ goalrecipe/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Goalrecipe::Application.config.secret_key_base = '0f32d557dae8ebb10d1ff56154c64ee6ddf7cb5e902aa62ea831a152594400c72e6b93c89174e7d0a7c2ac61ce1aa91821251dfc776f71157f632b7c28f84297'
